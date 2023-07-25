c19177c6785ad7a2bb73a05306790f82e3ae9a42
Alexandru <raducanu.alexandru2001@gmail.com>
2023-07-25 16:22:51 +0100

    Finished ex04

--- Labs/Ex04_Interfaces/src/qa/Circle.java
+++ Labs/Ex04_Interfaces/src/qa/Circle.java
@@ -1,6 +1,6 @@
 package qa;
 
-public class Circle extends Shape {
+public class Circle extends Shape implements Movable {
 
     private double radius;
 
@@ -35,5 +35,19 @@ public class Circle extends Shape {
     @Override
     public Point getCentrePoint() {
         return new Point(getX(), getY());
+    }
+
+    @Override
+    public Point getCurrentLocation() {
+        return getCentrePoint();
+    }
+
+    @Override
+    public void move(double x, double y) {
+        setX( getX() + x);
+        setY( getY() + y);
+
+        return;
+
     }
 }
--- Labs/Ex04_Interfaces/src/qa/Main.java
+++ Labs/Ex04_Interfaces/src/qa/Main.java
@@ -2,15 +2,28 @@ package qa;
 
 public class Main {
     public static void main(String[] args) {
+
+        Circle c = new Circle ("circle1", "red", 0,0, 10);
+
+        System.out.println("Circle location before move: " + c.getCurrentLocation());
+        c.move(10.5, 4.3);
+        System.out.println(("Circle location after move: " + c.getCurrentLocation()));
+        System.out.println();
+        
+
+        Circle c2 = new Circle ("circle2", "blue", 10,10, 5);
+
+
+
         Rectangle r1 = new Rectangle("Alex" , "red", 2, 10, 10 , 10 );
         Rectangle r2 = new Rectangle( "Void" , "none" , 0 , 0 , 0 , 0);
 
-        Circle c1 = new Circle( "Ballie" , "yellow",  10,10, 5);
+        Circle c11 = new Circle( "Ballie" , "yellow",  10,10, 5);
 
-        System.out.println(c1);
+        System.out.println(c11);
         System.out.println();
 
-        System.out.println("The area and the centre point are: " + c1.getCentrePoint() + " and " + c1.getArea() );
+        System.out.println("The area and the centre point are: " + c11.getCentrePoint() + " and " + c11.getArea() );
         System.out.println();
 
         System.out.println(r1);
