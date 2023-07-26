8a5b39ba0de42e14dff997a5f1ce0278bec371a0
Teodora Dumitrescu <teodoradt19@gmail.com>
2023-07-26 11:29:19 +0100

    main

--- src/com/qa/Main.java
+++ src/com/qa/Main.java
@@ -7,7 +7,7 @@ public class Main {
         /*
         Circle c = new Circle ("circle1", "red", 0,0, 10);
                               Circle c2 = new Circle ("circle2", "blue", 10,10, 5);
-                                         Rectangle r = new Rectangle ("rectangle1", "yellow", 0, 0, 10, 10);
+                                       cd   Rectangle r = new Rectangle ("rectangle1", "yellow", 0, 0, 10, 10);
                                                     Rectangle r2 = new Rectangle ("rectangle2", "green", 2, 2, 5, 2);
 
                                                                //print out the objects
