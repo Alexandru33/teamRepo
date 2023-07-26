e59fd70089740a674304dbf4377d32454e39bfb5
Teodora Dumitrescu <teodoradt19@gmail.com>
2023-07-26 09:31:43 +0100

    qa5 and 6

---
+++ src/com/qa/exceptions/TestCalc.java
@@ -0,0 +1,18 @@
+//Lab 6-1 Task 2 TestCalc Class with main()
+//Lab 6-1 Task 3 Add Exception Handling
+package com.qa.exceptions;
+
+public class TestCalc {
+    public static void main(String[] args) {
+        BadCalc bc = new BadCalc();
+        System.out.println("Mult result: " + bc.mult(2, 3));
+        try {
+            System.out.println("Div result: " + bc.div(6, 0));
+        } catch (ArithmeticException ae) {
+            System.out.println("Calculation error caught");
+            System.out.println(ae);
+            ae.printStackTrace();
+        }
+    }
+}
+
---
+++ src/com/qa/model/Dog.java
@@ -0,0 +1,26 @@
+package com.qa.model;
+public class Dog extends Animal {
+
+    public Dog(String name, int age) {
+       super(name, age);
+    }
+
+    @Override
+
+    public String sayHello() {
+        return "Oh hello! You're back! Hello! Hello! " ;
+                }
+
+
+    @Override
+
+    public String move() {
+        return "Ruuuuuuuuuuun!";
+       }
+
+
+
+    public String toString() {
+        return "Dog: " + super.toString();
+      }
+}
